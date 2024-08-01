b39302dfa02536c0f9e1235acf0df2f2eaa5639a
ayush-prajapati01 <aprajapti20ce@student.mes.ac.in>
2024-08-01 17:30:55 +0530

    [Ayush] Feat: Python Script to generate employee wage for multiple companies

--- employee.py
+++ employee.py
@@ -12,14 +12,27 @@ import random
 
 
 class EmployeeWage:
-    WAGE_PER_HOUR = 20
+    """
+    Description: 
+        This class is used for Employee Wage
+    Parameters:
+        wage_per_hour, working_days, max_working_hours
+    Return:
+        None
+    """
+
     FULL_TIME_HOUR = 8
     PART_TIME_HOUR = 4
-    WORKING_DAYS = 20
-    MAX_WORKING_HOURS = 100
 
-    @classmethod
-    def check_attendance(cls):
+
+    def __init__(self, company_name, wage_per_hour, working_days, max_working_hours):
+        self.company_name = company_name
+        self.WAGE_PER_HOUR = wage_per_hour
+        self.WORKING_DAYS = working_days
+        self.MAX_WORKING_HOURS = max_working_hours
+
+
+    def check_attendance(self):
         """
         Description: 
             This returns whether employee is present or absent
@@ -31,8 +44,7 @@ class EmployeeWage:
         return random.randint(0, 2)
 
 
-    @classmethod
-    def calculate_full_time_daily_wage(cls):
+    def calculate_full_time_daily_wage(self):
         """
         Description: 
             This function calculates full time daily wage of employee
@@ -41,11 +53,10 @@ class EmployeeWage:
         Return:
             int: daily full time wage
         """
-        return cls.FULL_TIME_HOUR * cls.WAGE_PER_HOUR
+        return self.FULL_TIME_HOUR * self.WAGE_PER_HOUR
 
 
-    @classmethod
-    def calculate_part_time_wage(cls):
+    def calculate_part_time_wage(self):
         """
         Description: 
             This function calculates part time wage of employee
@@ -54,11 +65,10 @@ class EmployeeWage:
         Return:
             int: daily part time wage
         """
-        return cls.PART_TIME_HOUR * cls.WAGE_PER_HOUR
+        return self.PART_TIME_HOUR * self.WAGE_PER_HOUR
 
 
-    @classmethod
-    def calculate_appropriate_daily_wage(cls, attendance):
+    def calculate_appropriate_daily_wage(self, attendance):
         """
         Description: 
             This function calculates appropriate wage of employee
@@ -70,15 +80,14 @@ class EmployeeWage:
         if attendance == 0:
             return 0
         elif attendance == 1:
-            return cls.calculate_full_time_daily_wage()
+            return self.calculate_full_time_daily_wage()
         elif attendance == 2:
-            return cls.calculate_part_time_wage()
+            return self.calculate_part_time_wage()
         else:
             return 0
 
 
-    @classmethod
-    def calculate_monthly_wage(cls):
+    def calculate_monthly_wage(self):
         """
         Description: 
             This function calculates the monthly wage of an employee
@@ -88,13 +97,12 @@ class EmployeeWage:
             int: monthly wage
         """
         employee_monthly_wage = 0
-        for _ in range(cls.WORKING_DAYS):
-            employee_monthly_wage += cls.calculate_appropriate_daily_wage(cls.check_attendance())
+        for _ in range(self.WORKING_DAYS):
+            employee_monthly_wage += self.calculate_appropriate_daily_wage(self.check_attendance())
         return employee_monthly_wage
 
 
-    @classmethod
-    def calculate_wage_by_hours_days(cls):
+    def calculate_wage_by_hours_days(self):
         """
         Description:
             This function calculates wages till a condition of total
@@ -108,25 +116,33 @@ class EmployeeWage:
         wage_by_hours_or_days = 0
         hours = days = 0
         daily_wage_list = []
-        while hours <= cls.MAX_WORKING_HOURS and days < cls.WORKING_DAYS:
-            attendance = cls.check_attendance()
+        while hours <= self.MAX_WORKING_HOURS and days < self.WORKING_DAYS:
+            attendance = self.check_attendance()
             if attendance == 1:
-                hours += cls.FULL_TIME_HOUR
+                hours += self.FULL_TIME_HOUR
             elif attendance == 2:
-                hours += cls.PART_TIME_HOUR
+                hours += self.PART_TIME_HOUR
 
             days += 1
-            daily_wage_list.append(cls.calculate_appropriate_daily_wage(attendance))
-            wage_by_hours_or_days += cls.calculate_appropriate_daily_wage(attendance)
+            daily_wage_list.append(self.calculate_appropriate_daily_wage(attendance))
+            wage_by_hours_or_days += self.calculate_appropriate_daily_wage(attendance)
             
-            if hours >= cls.MAX_WORKING_HOURS or days >= cls.WORKING_DAYS:
+            if hours >= self.MAX_WORKING_HOURS or days >= self.WORKING_DAYS:
                 break   
 
         return wage_by_hours_or_days, hours, days, daily_wage_list
 
 
-def main():
-    print("--- Welcome to Employee Wage Computation Program ---")
+def employee_wage_menu(company_name):
+    """
+    Description:
+        This function displays main menu for employee wage program
+    Parameters:
+        object: EmployeeWage class object
+    Return:
+        None
+    """
+    print(f"\nWelcome to {company_name.company_name}")
     while True:
         print("\nChoose an option:")
         print("0. Press 0 to exit")
@@ -141,16 +157,16 @@ def main():
                 break
         
             case 1:
-                attendance = EmployeeWage.check_attendance()
-                daily_wage = EmployeeWage.calculate_appropriate_daily_wage(attendance)
+                attendance = company_name.check_attendance()
+                daily_wage = company_name.calculate_appropriate_daily_wage(attendance)
                 print(f"Daily Wage: {daily_wage}")
             
             case 2:
-                monthly_wage = EmployeeWage.calculate_monthly_wage()
+                monthly_wage = company_name.calculate_monthly_wage()
                 print(f"Monthly Wage: {monthly_wage}")
             
             case 3:
-                conditional_wage, hours_worked, days_worked, daily_wage_list = EmployeeWage.calculate_wage_by_hours_days()
+                conditional_wage, hours_worked, days_worked, daily_wage_list = company_name.calculate_wage_by_hours_days()
                 print(f"Conditional Wage: {conditional_wage}")
                 print(f"Hours Worked by employee: {hours_worked}")
                 print(f"Days Worked by employee: {days_worked}")
@@ -158,6 +174,18 @@ def main():
             
             case _:
                 print("Invalid choice. Please enter 0, 1, 2, or 3.")
+
+
+def main():
+    print("--- Welcome to Employee Wage Computation Program ---")
+
+    # Creating Objects
+    company1 = EmployeeWage('TCS', 20, 20, 100)
+    # Display the menu for the user
+    employee_wage_menu(company1)
+
+    Infosys = EmployeeWage('Infosys', 30, 30, 150)
+    employee_wage_menu(Infosys)
 
 
 if __name__ == "__main__":
